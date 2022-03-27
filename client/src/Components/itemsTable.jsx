8355b3d891deb9f06575051d689bd78793f238fa
Germay1990 <shg1693@gmail.com>
2022-03-27 15:13:20 +0300

    .

--- client/src/Components/itemsTable.jsx
+++ client/src/Components/itemsTable.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import Item from "./item";
-import { ProgressBar } from "react-bootstrap";
-
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
   { field: "_id", headerName: "ID", width: 150 },
-  { field: "_title", headerName: "Title", width: 250 },
+  { field: "_title", headerName: "Title", width: 350 },
   { field: "_price", headerName: "Price", width: 150 },
 ];
 
